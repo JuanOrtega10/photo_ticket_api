16c105382af9069c2a6ea78d18f23198b72c680c
Juan Ortega <juanalbertoortega456@gmail.com>
2024-05-08 20:19:38 -0500

    `feat:` update Ticket serializer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
-from .models import Ticket, Image
+
+from .models import Image
+from .models import Ticket
+
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = '__all__'
+        fields = ['id', 'user', 'created_at', 'updated_at', 'status', 'total_images']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at', 'status']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Ticket.objects.create(user=user, status='pending', **validated_data)
+
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
