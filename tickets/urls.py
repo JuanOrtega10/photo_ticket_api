b0a4a6c7898b5c63158828072e94fd2d8eede898
Juan Ortega <juanalbertoortega456@gmail.com>
2024-05-08 20:20:17 -0500

    `feat:` update urls to match api expectations

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,11 +1,18 @@
 from rest_framework.routers import DefaultRouter
-from .views import TicketViewSet, ImageViewSet
+from .views import TicketViewSet, TicketImageViewSet, TicketDetailViewSet
 from django.urls import include, path
 
 router = DefaultRouter()
-router.register(r'tickets', TicketViewSet, basename='ticket')
-router.register(r'images', ImageViewSet, basename='image')
+router.register(r"tickets", TicketViewSet, basename="tickets")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    # Specific route for uploading images to a ticket
+    path("v1/tickets/<int:ticket_id>/images/", TicketImageViewSet.as_view({
+        "post": "create"
+    }), name="ticket-images"),
+    # Specific route for viewing details of a ticket
+    path("v1/tickets/<int:ticket_id>/", TicketDetailViewSet.as_view({
+        "get": "retrieve"
+    }), name="ticket-detail"),
 ]
