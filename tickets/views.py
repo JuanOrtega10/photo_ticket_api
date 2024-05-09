11441bff192d91f61e762dcd5bb35e296342ec1c
Juan Ortega <juanalbertoortega456@gmail.com>
2024-05-08 20:20:29 -0500

    `feat:` update views to match api expectations

--- tickets/views.py
+++ tickets/views.py
@@ -1,22 +1,51 @@
-from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import Ticket, Image
 from .serializers import TicketSerializer, ImageSerializer
+
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.none()
     serializer_class = TicketSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Ticket.objects.filter(user=self.request.user)
+        queryset = Ticket.objects.filter(user=self.request.user)
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        status = self.request.query_params.get('status')
 
-class ImageViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.none()
+        if start_date:
+            queryset = queryset.filter(created_at__gte=start_date)
+        if end_date:
+            queryset = queryset.filter(created_at__lte=end_date)
+        if status:
+            queryset = queryset.filter(status=status)
+
+        return queryset
+class TicketImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.none()
     serializer_class = ImageSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Image.objects.filter(ticket__user=self.request.user)
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(ticket=Ticket.objects.filter(user=self.request.user, id=self.request.data.get('ticket')).first())
+        ticket_id = self.kwargs.get('ticket_id')
+        ticket = get_object_or_404(Ticket, pk=ticket_id)
+        serializer.save(ticket=ticket)
+
+class TicketDetailViewSet(viewsets.GenericViewSet):
+    queryset = Ticket.objects.none()
+    serializer_class = TicketSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
