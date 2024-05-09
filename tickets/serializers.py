fba1202fe9f0ce32a4cae36a181849585694127a
Juan Ortega <juanalbertoortega456@gmail.com>
2024-05-08 23:40:07 -0500

    `feat:` update serializers to show image details in the ticket object

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -4,18 +4,20 @@ from .models import Image
 from .models import Ticket
 
 
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = ['id', 'user', 'created_at', 'updated_at', 'status', 'total_images']
-        read_only_fields = ['id', 'user', 'created_at', 'updated_at', 'status']
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        return Ticket.objects.create(user=user, status='pending', **validated_data)
-
-
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = '__all__'
+        fields = ['id', 'image_url', 'public_id', 'ticket', 'uploaded_at']
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = ['id', 'user', 'created_at', 'updated_at', 'status', 'total_images', 'current_images', 'images']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at', 'status', 'current_images', 'images']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Ticket.objects.create(user=user, status='pending', current_images=0, **validated_data)
